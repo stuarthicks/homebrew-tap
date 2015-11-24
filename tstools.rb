cd51cbbcefdbcc7e4df0f2abd271c7dea13d5ef4
Stuart Hicks <stuarthicks@users.noreply.github.com>
2015-11-24 10:27:58 +0000

    bump tstools, now places executables into bin

--- tstools.rb
+++ tstools.rb
@@ -2,9 +2,9 @@ class Tstools < Formula
   homepage "https://github.com/stuarthicks/tstools"
   desc "fork of kynesim/tstools using cmake for easier building on modern osx"
 
-  url "https://github.com/stuarthicks/tstools/archive/0.0.1.tar.gz"
-  sha256 "b68bdd4828efe1d3dc5ea3cbfaf51183eec2c031e9374a9fb0cb84a792a743ff"
-  version "0.0.1"
+  url "https://github.com/stuarthicks/tstools/archive/0.0.2.tar.gz"
+  sha256 "2ebf8bcecd561349edeedba771019e9582e45897e28f54804e25cb930c7a158c"
+  version "0.0.2"
 
   depends_on "cmake" => :build
 
