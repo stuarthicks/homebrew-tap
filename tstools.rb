d1718e22f75472dcf4fa40b80aecc312d241e678
Stuart Hicks <stuarthicks@users.noreply.github.com>
2015-12-01 09:25:43 +0000

    Update tstools.rb

--- tstools.rb
+++ tstools.rb
@@ -1,7 +1,7 @@
 class Tstools < Formula
   homepage "https://github.com/stuarthicks/tstools"
 
-  url "https://github.com/stuarthicks/tstools/archive/0.0.2.tar.gz"
+  url "https://github.com/stuarthicks/tstools/archive/0.0.3.tar.gz"
   sha256 "bab3ccf5398d64f35c69a7e7611af254bf96e6cb4af741641d89da2ac2327247"
   version "0.0.2"
 
